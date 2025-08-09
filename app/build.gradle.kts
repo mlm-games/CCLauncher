001294f5929947c10cdee59c12e88aa6dd28e2ba
ragebreaker <125530737+mlm-games@users.noreply.github.com>
2025-08-09 10:15:43 +0530

    Update build.gradle.kts to increment x.ver.x

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 800
-        versionName = "v9.8.1"
+        versionName = "v9.9.0"
 
         androidResources {
             localeFilters += setOf("en", "ar", "de", "es-rES", "es-rUS", "fr", "hr", "hu", "in", "it", "ja", "pl", "pt-rBR", "ru-rRU", "sv", "tr", "uk", "zh")
