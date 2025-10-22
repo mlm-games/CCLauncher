317ab8080d07ac36b443a534588c860e157b7249
ragebreaker <125530737+mlm-games@users.noreply.github.com>
2025-10-22 18:02:33 +0530

    Revert versionCode and versionName in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "app.cclauncher"
         minSdk = 24
         targetSdk = 36
-        versionCode = 890
-        versionName = "v9.9.9"
+        versionCode = 880
+        versionName = "v9.9.8"
 
         androidResources {
             localeFilters += setOf("en", "ar", "de", "es-rES", "es-rUS", "fr", "hr", "hu", "in", "it", "ja", "pl", "pt-rBR", "ru-rRU", "sv", "tr", "uk", "zh")
