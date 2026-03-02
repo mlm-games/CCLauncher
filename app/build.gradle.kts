1bba4431ff48ff4d0f878a1d1497d10b352c6fff
ragebreaker <125530737+mlm-games@users.noreply.github.com>
2026-03-02 07:51:21 +0530

    chore: bump ver

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 1230
-        versionName = "v10.6.4"
+        versionName = "v10.7.0"
 
         androidResources {
             localeFilters += setOf("en", "ar", "de", "es-rES", "es-rUS", "fr", "hr", "hu", "in", "it", "ja", "pl", "pt-rBR", "ru-rRU", "sv", "tr", "uk", "zh")
